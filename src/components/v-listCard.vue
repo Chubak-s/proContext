7f79e7dc1b938b424778fdabffe51d00fd278ab0
Chubak <gameproclass@gmail.com>
2024-04-26 02:51:32 +0300

    final commit

--- src/components/v-listCard.vue
+++ src/components/v-listCard.vue
@@ -1,7 +1,6 @@
 <script>
 import VItem from "@/components/v-item.vue";
 import store from "@/vuex/store.js";
-import {createApp} from "vue";
 
 export default {
   name: 'v-listCard',
@@ -25,37 +24,42 @@ export default {
     },
     createContent() {
       if (this.option==='sort'){
-        const content = document.getElementById(`${this.list.id}dropdown-content`)
-        content.innerHTML=''
+        const content = document.getElementById(`${this.list.id}dropdown-content`);
+        content.innerHTML = '';
         content.style.display = 'flex';
-        content.style.flexDirection = 'column'
+        content.style.flexDirection = 'column';
         content.style.gap = '10px';
-        for (let item of store.state.listsResult[this.list.id-1]) {
-          const divItem = document.createElement('div')
+
+        const items = store.state.listsResult[this.list.id - 1];
+        items.forEach((item) => {
+          const divItem = document.createElement('div');
           divItem.style.display = 'flex';
           divItem.style.gap = '5px';
           divItem.style.width = '95%';
-          divItem.style.flexWrap = 'wrap'
-          for (let j=0; j<item.count; j++){
-            const divColorBox = document.createElement('div')
-            divColorBox.classList.add('colorBox')
-            divColorBox.addEventListener('click', function() {
-              console.log(this);
-            });
-            divColorBox.innerText='ㅤ'
-            divColorBox.style.backgroundColor = item.color
-            divColorBox.style.height = '15px'
-            divColorBox.style.width = '15px'
-            divItem.appendChild(divColorBox)
+          divItem.style.flexWrap = 'wrap';
+
+          for (let j = 0; j < item.count; j++) {
+            const divColorBox = document.createElement('div');
+            divColorBox.classList.add('colorBox');
+            divColorBox.innerText = 'ㅤ';
+            divColorBox.style.cursor = 'pointer'
+            divColorBox.style.backgroundColor = item.color;
+            divColorBox.style.height = '15px';
+            divColorBox.style.width = '15px';
+            divColorBox.addEventListener('click', () => this.removeColorBox(divColorBox));
+            divItem.appendChild(divColorBox);
           }
-          content.appendChild(divItem)
-        }
+
+          content.appendChild(divItem);
+        });
       }
       else if(this.option === 'random'){
         const content = document.getElementById(`${this.list.id}dropdown-content`)
+
         content.innerHTML=''
         content.style.display = 'flex';
         content.style.flexDirection = 'row'
+
         let array = []
         for (let item of store.state.listsResult[this.list.id-1]) {
           for (let j=0; j<item.count; j++) {
@@ -65,19 +69,49 @@ export default {
         function shuffle(a, b) {
           return 0.5 - Math.random();
         }
-
-// Перемешивание порядка элементов в массиве
         const shuffledArray =array.sort(shuffle);
-        console.log(shuffledArray)
+
         for (let j=0; j<shuffledArray.length; j++){
             const divColorBox = document.createElement('div')
             divColorBox.innerText='ㅤ'
+            divColorBox.style.cursor = 'pointer'
             divColorBox.style.backgroundColor = shuffledArray[j]
             divColorBox.style.height = '15px'
             divColorBox.style.width = '15px'
+            divColorBox.addEventListener('click', () => this.removeColorBox(divColorBox));
             content.appendChild(divColorBox)
         }
       }
+    },
+    rgbToHex(rgb) {
+      // Удаляем "rgb(" и ")" из строки
+      let rgbValues = rgb.replace('rgb(', '').replace(')', '').split(',');
+
+      // Преобразуем значения в числа
+      let r = parseInt(rgbValues[0].trim());
+      let g = parseInt(rgbValues[1].trim());
+      let b = parseInt(rgbValues[2].trim());
+
+      // Преобразуем каждое значение в шестнадцатеричный формат
+      let rHex = r.toString(16).toUpperCase().padStart(2, '0');
+      let gHex = g.toString(16).toUpperCase().padStart(2, '0');
+      let bHex = b.toString(16).toUpperCase().padStart(2, '0');
+
+      // Объединяем все значения в один HEX-код
+      let hexColor = `#${rHex}${gHex}${bHex}`;
+      return hexColor;
+    },
+    removeColorBox(colorBox) {
+      const color = this.rgbToHex(colorBox.style.backgroundColor)
+      console.log(color)
+      const items = store.state.listsResult[this.list.id - 1];
+      items.forEach((item) => {
+        console.log(item.color)
+        if (item.color === color){
+          item.count--;
+        }
+      })
+      colorBox.remove();
     },
     sorting(){
       const el = document.getElementById(`${this.list.id}function`)
